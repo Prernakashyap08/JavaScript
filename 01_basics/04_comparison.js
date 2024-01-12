465ce05827672f5b7d7ac8e44807a6e2bf9f159c
PRERNA RANI <84430535+Prernakashyap08@users.noreply.github.com>
2024-01-12 19:07:46 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+console.log( 2  == 2)
+console.log( 2  != 2)
+console.log( 2  >= 2)
+console.log( 2  <= 2)
+
+console.log( "2" >= 1)
+console.log( "02" >= 1)
+
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+
+
+console.log(undefined > 0)
+console.log(undefined == 0)
+console.log(undefined >= 0 )
+
+
+
+// ===   it compare the numbers as well as the data types of the two
+console.log("2" === 2);
